b82ef9a1bc6df02247b590262af36874de845273
Wolfgang Mayer <wolfgang.mayer@engel.at>
2024-11-17 12:21:09 +0100

    fixing project name mismatch

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ const REALTIME_TIMESTAMP: &str = "__REALTIME_TIMESTAMP";
 const SOURCE_REALTIME_TIMESTAMP: &str = "_SOURCE_REALTIME_TIMESTAMP";
 
 fn main() {
-    let mut journal_stream = rust_journald::journal_stream_args(env::args()).unwrap_or_else(|err| {
-        eprintln!("Couldn't create journal iterator: {err}");
-        process::exit(1);
-    });
+    let mut journal_stream = journald_otel_collector::journal_stream_args(env::args())
+        .unwrap_or_else(|err| {
+            eprintln!("Couldn't create journal iterator: {err}");
+            process::exit(1);
+        });
 
     journal_stream.for_each(|entry| {
         let message = entry.get_message().unwrap_or("<no-message>");
